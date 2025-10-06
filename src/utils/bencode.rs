4daec88ee149c016405cad3f77a030b73432658a
slavonu4 <slavonu4@gmail.com>
2025-10-06 11:52:12 +0300

    Support negative integers

--- src/utils/bencode.rs
+++ src/utils/bencode.rs
@@ -3,7 +3,7 @@ use lazy_static::lazy_static;
 use regex::Regex;
 
 lazy_static! {
-    pub static ref BENCODED_INT_REGEXP: Regex = Regex::new(r"i(?<value>\d+)e")
+    pub static ref BENCODED_INT_REGEXP: Regex = Regex::new(r"i(?<value>-?\d+)e")
         .context("Unable to create bencoded integer pattern matcher")
         .unwrap();
     pub static ref BENCODED_STR_REGEXP: Regex = Regex::new(r"(?<len>\d+):(?<value>\S+)")
